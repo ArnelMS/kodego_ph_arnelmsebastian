31d9c19cc70740c0240577e0044c315f9a0cd421
Arnel Sebastian <arnelmsebastian@gmail.com>
2022-11-08 23:40:02 +0800

    Activity01-A

--- src/main/kotlin/assignements/kodego_ph_activity01a_sebastian.kt
+++ src/main/kotlin/assignements/kodego_ph_activity01a_sebastian.kt
@@ -1,1 +1,48 @@
-fun main()
+package assignements
+
+fun main() {
+
+//    You are tasked to create a directory of the students taking this course.
+//    The data will be used to send updates regarding the classes.
+//    There is also a need to know the progress of the students during the progress of this course.
+//    List down all data that can be used to implement this. Use the proper data types.
+
+    var name1 : String = "Juan Dela Cruz"
+    var course1 : String = "Mobile Development"
+    var level1 : String = "First Year"
+    var email1 : String = "Juan@email.com"
+    var mobile1 : String = "091234567891"
+    var average1 : Int = 85
+    var attendance1 : String = "100%"
+
+    var name2 : String = "Pepito Manaloto"
+    var course2 : String = "Mobile Development"
+    var level2 : String = "First Year"
+    var email2 : String = "Pepito@email.com"
+    var mobile2 : String = "09998765432"
+    var average2 : Int = 93
+    var attendance2 : String = "86%"
+
+    println("------------------------------------")
+    println("Student 1")
+    println(name1)
+    println(course1)
+    println(level1)
+    println(email1)
+    println(mobile1)
+    println(average1)
+    println(attendance1)
+
+    println("------------------------------------")
+    println("Student 1")
+    println(name2)
+    println(course2)
+    println(level2)
+    println(email2)
+    println(mobile2)
+    println(average2)
+    println(attendance2)
+    println("------------------------------------")
+    println("- End of Code -")
+
+}
